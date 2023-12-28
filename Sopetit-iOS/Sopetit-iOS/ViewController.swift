feb9c102c889bd33f7f3e12b35870f2c9eeb1de9
ahra1221 <lizze011221@gmail.com>
2023-12-28 23:25:44 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ Sopetit-iOS/Sopetit-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sopetit-iOS
+//
+//  Created by 고아라 on 2023/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
