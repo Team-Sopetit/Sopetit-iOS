009e6d3f4989548f2ea252b8ff2265993df3567e
ahra1221 <lizze011221@gmail.com>
2024-01-01 20:35:50 +0900

    [Fix] #1 - 파일 위치 변경

---
+++ Sopetit-iOS/Sopetit-iOS/Global/Literals/StringLiterals.swift
@@ -0,0 +1,14 @@
+//
+//  StringLiterals.swift
+//  Sopetit-iOS
+//
+//  Created by 고아라 on 2023/12/29.
+//
+
+import Foundation
+
+enum I18N {
+    enum Common {
+        static let nextButtonTitle = "다음으로"
+    }
+}
