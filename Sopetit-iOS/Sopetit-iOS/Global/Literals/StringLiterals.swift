b6005bac2d7989e32e07f8e48992ebc30e6a2c9b
ahra1221 <lizze011221@gmail.com>
2024-01-08 02:05:27 +0900

    [Feat] #4 - BottomSheetType 구현

--- Sopetit-iOS/Sopetit-iOS/Global/Literals/StringLiterals.swift
+++ Sopetit-iOS/Sopetit-iOS/Global/Literals/StringLiterals.swift
@@ -8,7 +8,18 @@
 import Foundation
 
 enum I18N {
-    enum Common {
-        static let nextButtonTitle = "다음으로"
+    enum BottomSheet {
+        static let dailyTitle = "데일리 루틴을 완료했나요?"
+        static let happyTitle = "행복 루틴을 완료했나요?"
+        static let delTitle = "정말 삭제할까요?"
+        static let logoutTitle = "정말 로그아웃 할까요?"
+        static let emptyTitle = ""
+        static let delSubTitle = "루틴을 삭제해도 달성 횟수는 저장돼요"
+        static let logoutSubTitle = "\"잠시 안녕.. 다음에 또 봐!\""
+        static let dailyLeftTitle = "아니, 아직이야"
+        static let delLeftTitle = "취소"
+        static let dailyRightTitle = "완료했어"
+        static let delRightTitle = "삭제할래"
+        static let logoutRightTitle = "로그아웃 할래"
     }
 }
