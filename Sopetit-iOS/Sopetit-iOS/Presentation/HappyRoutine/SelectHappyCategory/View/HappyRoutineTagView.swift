88c851938092e52496e704bb3bf449b69585f6b4
frohsch <kminjoo1213@gmail.com>
2024-01-10 10:42:19 +0900

    [Feat] #20 - Implement Happy Routine Tag View

--- Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/SelectHappyCategory/View/HappyRoutineTagView.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/SelectHappyCategory/View/HappyRoutineTagView.swift
@@ -10,17 +10,28 @@ import UIKit
 import SnapKit
 
 final class HappyRoutineTagView: UIView {
-
-    // MARK: - Properties
     
+    // MARK: - Properties
     
     // MARK: - UI Components
     
+    let collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 6
+        layout.sectionInset = UIEdgeInsets(top: 12, left: 20, bottom: 12, right: 20)
+        layout.scrollDirection = .horizontal
+        collectionView.isScrollEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.collectionViewLayout = layout
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
     
     // MARK: - Life Cycles
     
-    override init(frame: CTFrame, collectionViewLayout: UICollectionViewLayout) {
-        super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
         setUI()
         setHierarchy()
@@ -38,21 +49,24 @@ final class HappyRoutineTagView: UIView {
 // MARK: - Extensions
 
 extension HappyRoutineTagView {
-
+    
     func setUI() {
         
     }
     
     func setHierarchy() {
-
+        self.addSubviews(collectionView)
     }
     
     func setLayout() {
-
+        collectionView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalTo(61)
+        }
     }
     
     func setAddTarget() {
-
+        
     }
     
     @objc
@@ -61,7 +75,7 @@ extension HappyRoutineTagView {
     }
     
     func setRegisterCell() {
-        
+        collectionView.register(HappyRoutineTagCollectionViewCell.self, forCellWithReuseIdentifier: "HappyRoutineTagCollectionViewCell")
     }
     
     func setDataBind() {
