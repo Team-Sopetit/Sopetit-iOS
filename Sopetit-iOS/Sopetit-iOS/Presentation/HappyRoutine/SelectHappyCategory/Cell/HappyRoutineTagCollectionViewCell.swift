e7304039db74f1989f3534277ea15c67eb9bfbf6
frohsch <kminjoo1213@gmail.com>
2024-01-10 10:41:40 +0900

    [Feat] #20 - Implement Top tag cell

--- Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/SelectHappyCategory/Cell/HappyRoutineTagCollectionViewCell.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/SelectHappyCategory/Cell/HappyRoutineTagCollectionViewCell.swift
@@ -15,8 +15,23 @@ final class HappyRoutineTagCollectionViewCell: UICollectionViewCell, UICollectio
 
     static let isFromNib: Bool = false
     
-    // MARK: - UI Components
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                bindSelected()
+            } else {
+                bindNotSelected()
+            }
+        }
+    }
     
+    // MARK: - UI Components
+    let tagLabel: UILabel = {
+        let label = UILabel()
+        label.font = .fontGuide(.body4)
+        label.textColor = .Gray400
+        return label
+     }()
     
     // MARK: - Life Cycles
     
@@ -31,21 +46,51 @@ final class HappyRoutineTagCollectionViewCell: UICollectionViewCell, UICollectio
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        tagLabel.text = ""
+        bindNotSelected()
+    }
 }
 
 // MARK: - Extensions
 
 extension HappyRoutineTagCollectionViewCell {
+    
+    private func setUI() {
+        contentView.backgroundColor = .SoftieWhite
+        contentView.layer.masksToBounds = true
+        contentView.layer.cornerRadius = 37/2
+        contentView.layer.borderColor = UIColor.Gray100.cgColor
+        contentView.layer.borderWidth = 1
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(tagLabel)
+    }
+    
+    private func setLayout() {
+        tagLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
 
-    func setUI() {
-        
+extension HappyRoutineTagCollectionViewCell {
+    func bindText(text: String) {
+        tagLabel.text = text
     }
     
-    func setHierarchy() {
-        
+    func bindSelected() {
+        contentView.backgroundColor = .SoftieMain1
+        contentView.layer.borderColor = UIColor.SoftieMain1.cgColor
+        tagLabel.textColor = .SoftieWhite
     }
     
-    func setLayout() {
-        
+    func bindNotSelected() {
+        contentView.backgroundColor = .SoftieWhite
+        contentView.layer.borderColor = UIColor.Gray100.cgColor
+        tagLabel.textColor = .Gray400
     }
 }
