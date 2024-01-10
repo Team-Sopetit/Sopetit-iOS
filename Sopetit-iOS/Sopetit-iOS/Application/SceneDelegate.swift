d39a9d9d4961eec43f1f3a737fcbce8c87f88a52
frohsch <kminjoo1213@gmail.com>
2024-01-10 10:45:20 +0900

    [Chore] #20 - Set SceneDelegate

--- Sopetit-iOS/Sopetit-iOS/Application/SceneDelegate.swift
+++ Sopetit-iOS/Sopetit-iOS/Application/SceneDelegate.swift
@@ -25,7 +25,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let rootVC = UINavigationController(rootViewController: TabBarController())
+        let rootVC = UINavigationController(rootViewController: HappyRoutineViewController())
         
         window.rootViewController = rootVC
         window.makeKeyAndVisible()
