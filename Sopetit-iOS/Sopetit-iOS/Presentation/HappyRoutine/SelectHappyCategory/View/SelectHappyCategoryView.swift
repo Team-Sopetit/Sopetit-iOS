fb273db8622edbb75351aebe6df1e10d2b0fe343
frohsch <kminjoo1213@gmail.com>
2024-01-10 10:43:42 +0900

    [Feat] #20 - Implement AddHappyRoutineCategory View

--- Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/SelectHappyCategory/View/SelectHappyCategoryView.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/SelectHappyCategory/View/SelectHappyCategoryView.swift
@@ -13,9 +13,21 @@ final class SelectHappyCategoryView: UIView {
 
     // MARK: - Properties
     
-    
     // MARK: - UI Components
+    private let customNavigationBar: CustomNavigationBarView = {
+        let view = CustomNavigationBarView()
+        view.isTitleViewIncluded = true
+        view.isTitleLabelIncluded = I18N.HappyRoutine.addHappyRoutineTitle
+        view.isBackButtonIncluded = true
+        return view
+    }()
     
+    let tagview = HappyRoutineTagView()
+    private let divideView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .Gray100
+        return view
+    }()
     
     // MARK: - Life Cycles
     
@@ -40,15 +52,32 @@ final class SelectHappyCategoryView: UIView {
 extension SelectHappyCategoryView {
 
     func setUI() {
-        
+        self.backgroundColor = .SoftieBack
     }
     
     func setHierarchy() {
+        self.addSubviews(customNavigationBar, tagview, divideView)
 
     }
     
     func setLayout() {
-
+        customNavigationBar.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        tagview.snp.makeConstraints {
+            $0.top.equalTo(customNavigationBar.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(61)
+        }
+        
+        divideView.snp.makeConstraints {
+            $0.top.equalTo(tagview.snp.bottom)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
     }
     
     func setAddTarget() {
