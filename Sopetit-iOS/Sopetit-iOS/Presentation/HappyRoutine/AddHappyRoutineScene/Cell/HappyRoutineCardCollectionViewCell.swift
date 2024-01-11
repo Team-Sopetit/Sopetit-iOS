b0b18f4eac2bf55c7be4434c8f1ba662edbb7779
frohsch <kminjoo1213@gmail.com>
2024-01-11 09:18:57 +0900

    [Feat] #20 - Add HappyRoutine Model and Dummy

---
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/AddHappyRoutineScene/Cell/HappyRoutineCardCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  HappyRoutineCardCollectionViewCell.swift
+//  Sopetit-iOS
+//
+//  Created by Minjoo Kim on 1/10/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HappyRoutineCardCollectionViewCell: UICollectionViewCell, UICollectionViewRegisterable {
+    
+    // MARK: - Properties
+
+    static let isFromNib: Bool = false
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension HappyRoutineCardCollectionViewCell {
+
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+}
---
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/AddHappyRoutineScene/ViewController/AddHappyRoutineViewController.swift
@@ -0,0 +1,64 @@
+//
+//  AddHappyRoutineViewController.swift
+//  Sopetit-iOS
+//
+//  Created by Minjoo Kim on 1/10/24.
+//
+
+import UIKit
+
+final class AddHappyRoutineViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        super.loadView()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getAPI()
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+    }
+}
+
+// MARK: - Extensions
+
+extension AddHappyRoutineViewController {
+
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setDelegate() {
+        
+    }
+}
+
+// MARK: - Network
+
+extension AddHappyRoutineViewController {
+
+    func getAPI() {
+        
+    }
+}
