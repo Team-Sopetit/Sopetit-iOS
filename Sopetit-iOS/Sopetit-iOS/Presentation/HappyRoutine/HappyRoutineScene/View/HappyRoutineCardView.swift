1ce465991e6f2497b751494c3554ef20fa8d11ab
frohsch <kminjoo1213@gmail.com>
2024-01-11 10:53:13 +0900

    [Feat] #32 - Add UserHappyRoutine Model and Dummy

---
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/HappyRoutineScene/View/HappyRoutineCardView.swift
@@ -0,0 +1,70 @@
+//
+//  HappyRoutineCardView.swift
+//  Sopetit-iOS
+//
+//  Created by Minjoo Kim on 1/11/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HappyRoutineCardView: UIView {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setAddTarget()
+        setRegisterCell()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension HappyRoutineCardView {
+
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+
+    }
+    
+    func setLayout() {
+
+    }
+    
+    func setAddTarget() {
+
+    }
+    
+    @objc
+    func buttonTapped() {
+        
+    }
+    
+    func setRegisterCell() {
+        
+    }
+    
+    func setDataBind() {
+        
+    }
+}
---
+++ Sopetit-iOS/Sopetit-iOS/Presentation/HappyRoutine/HappyRoutineScene/View/HappyRoutineView.swift
@@ -0,0 +1,70 @@
+//
+//  HappyRoutineView.swift
+//  Sopetit-iOS
+//
+//  Created by Minjoo Kim on 1/11/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class HappyRoutineView: UIView {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setAddTarget()
+        setRegisterCell()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Extensions
+
+extension HappyRoutineView {
+
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+
+    }
+    
+    func setLayout() {
+
+    }
+    
+    func setAddTarget() {
+
+    }
+    
+    @objc
+    func buttonTapped() {
+        
+    }
+    
+    func setRegisterCell() {
+        
+    }
+    
+    func setDataBind() {
+        
+    }
+}
