6605c034331cde39cc645721c83d9cbd9df3f22b
ahra1221 <lizze011221@gmail.com>
2024-01-12 13:47:01 +0900

    [Setting] #40 - BaseURL 세팅

--- Sopetit-iOS/Sopetit-iOS/Network/Environment/Config.swift
+++ Sopetit-iOS/Sopetit-iOS/Network/Environment/Config.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum Config {
+    
+    enum Keys {
+        enum Plist {
+            static let baseURL = "BASE_URL"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found.")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    
+    // MARK: Base URL
+    
+    static let baseURL: String = {
+        guard let url = Config.infoDictionary[Keys.Plist.baseURL] as? String else {
+            fatalError("Base URL is not set in plist for this configuration.")
+        }
+        return url
+    }()
+}
