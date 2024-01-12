bf4badc1e7f12c47f722caa3d97496fe86738ec1
ahra1221 <lizze011221@gmail.com>
2024-01-12 13:47:55 +0900

    [Setting] #40 - 네트워크 세팅

--- Sopetit-iOS/Sopetit-iOS/Network/Base/BaseService.swift
+++ Sopetit-iOS/Sopetit-iOS/Network/Base/BaseService.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+import Alamofire
+
+class BaseService {
+    func judgeStatus<T: Codable>(by statusCode: Int, _ data: Data, _ t: T.Type) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(GenericResponse<T>.self, from: data) else { return .pathErr }
+        switch statusCode {
+        case 200..<300: return .success(decodedData as Any)
+        case 400..<500: return .requestErr(decodedData as Any)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+}
