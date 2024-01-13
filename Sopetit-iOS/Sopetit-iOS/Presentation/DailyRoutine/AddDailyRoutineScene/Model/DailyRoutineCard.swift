d92465160df1c4f65ad21d45df9f7ecf2139a662
frohsch <kminjoo1213@gmail.com>
2024-01-13 19:16:32 +0900

    [Feat] #44 - Add DailyRoutine Model and Dummy

--- Sopetit-iOS/Sopetit-iOS/Presentation/DailyRoutine/AddDailyRoutineScene/Model/DailyRoutineCard.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/DailyRoutine/AddDailyRoutineScene/Model/DailyRoutineCard.swift
@@ -5,4 +5,23 @@
 //  Created by Minjoo Kim on 1/13/24.
 //
 
-import Foundation
+import UIKit
+
+struct DailyRoutineCard {
+    let routineId: Int
+    let content: String
+}
+
+extension DailyRoutineCard {
+    
+    func dummy() -> [DailyRoutineCard] {
+        return [DailyRoutineCard(routineId: 0, content: "일어나면\n5분 안에 이불 개기"),
+                DailyRoutineCard(routineId: 1, content: "일어나면\n5분 안에 이불 개기"),
+                DailyRoutineCard(routineId: 2, content: "일어나면\n5분 안에 이불 개기"),
+                DailyRoutineCard(routineId: 3, content: "일어나면\n5분 안에 이불 개기"),
+                DailyRoutineCard(routineId: 4, content: "일어나면\n5분 안에 이불 개기"),
+                DailyRoutineCard(routineId: 5, content: "일어나면\n5분 안에 이불 개기"),
+                DailyRoutineCard(routineId: 6, content: "일어나면\n5분 안에 이불 개기"),
+        ]
+    }
+}
