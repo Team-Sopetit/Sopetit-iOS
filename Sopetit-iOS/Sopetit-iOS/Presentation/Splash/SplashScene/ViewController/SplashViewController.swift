e4855e3b84eb0d04c18eaaaaa454245463b7c096
timotheekim10 <timotheekim10@gmail.com>
2024-01-14 19:12:02 +0900

    [Feat] #43 - 스플래시 뷰 일정시간 후 로그인 뷰로 이동 구현

--- Sopetit-iOS/Sopetit-iOS/Presentation/Splash/SplashScene/ViewController/SplashViewController.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/Splash/SplashScene/ViewController/SplashViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-final class SplashOneViewController: UIViewController {
+final class SplashViewController: UIViewController {
     
     // MARK: - Properties
     
@@ -21,5 +21,22 @@ final class SplashOneViewController: UIViewController {
     
     override func loadView() {
         self.view = splashViews[randomNumber]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 스플래시 뷰에서 표시할 내용 추가
+        // 예: 배경 이미지, 로고 등
+        
+        // 일정 시간 후에 다음 화면으로 전환
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // 2.0초 후에 다음 화면으로 전환
+            self.navigateToNextScreen()
+        }
+    }
+    
+    private func navigateToNextScreen() {
+        let nav = LoginViewController()
+        self.navigationController?.pushViewController(nav, animated: false)
     }
 }
