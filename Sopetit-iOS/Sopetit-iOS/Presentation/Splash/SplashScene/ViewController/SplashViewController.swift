3d6d0f47185a03b1fb858f427d6b3cfa84c27f24
ahra1221 <lizze011221@gmail.com>
2024-01-31 04:10:22 +0900

    [Feat] #117 - API 연결 및 로직 구현

--- Sopetit-iOS/Sopetit-iOS/Presentation/Splash/SplashScene/ViewController/SplashViewController.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/Splash/SplashScene/ViewController/SplashViewController.swift
@@ -7,29 +7,157 @@
 
 import UIKit
 
+import SnapKit
+
+enum AppstoreOpenError: Error {
+    case invalidAppStoreURL
+    case cantOpenAppStoreURL
+}
+
 final class SplashViewController: UIViewController {
     
     // MARK: - Properties
     
     lazy var randomNumber: Int = Int.random(in: (0 ..< splashViews.count))
+    private var versionEntity = VersionEntity(iosVersion: Version(appVersion: "", forceUpdateVersion: ""), androidVersion: Version(appVersion: "", forceUpdateVersion: ""), notificationTitle: "", notificationContent: "")
+    private let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    private let appStoreOpenUrlString = "itms-apps://itunes.apple.com/app/apple-store/6476357728"
     
     // MARK: - UI Components
     
     private let splashViews: [UIView] = [SplashOneView(), SplashTwoView(), SplashThreeView(), SplashFourView()]
+    private let updateAlertView = UpdateAlertView()
     
     // MARK: - Life Cycles
     
     override func loadView() {
+        
         self.view = splashViews[randomNumber]
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.2) {
-            self.showNextPage()
+            self.getVersionAPI()
         }
     }
 }
+
+private extension SplashViewController {
+    
+    func setUI() {
+        self.updateAlertView.isHidden = true
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    func setDelegate() {
+        updateAlertView.alertDelegate = self
+    }
+    
+    func setHierarchy() {
+        self.view.addSubview(updateAlertView)
+    }
+    
+    func setLayout() {
+        updateAlertView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func openAppStore(urlStr: String) -> Result<Void, AppstoreOpenError> {
+        guard let url = URL(string: urlStr) else {
+            return .failure(.invalidAppStoreURL)
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            return .success(())
+        } else {
+            return .failure(.cantOpenAppStoreURL)
+        }
+    }
+    
+    func showUpdateAlert(forceResult: Int, recommendResult: Int) {
+        if forceResult < 0 {
+            updateAlertView.isHidden = false
+            updateAlertView.setDataBind(updateCase: "force", model: self.versionEntity)
+        } else if recommendResult <= 0 {
+            updateAlertView.isHidden = false
+            updateAlertView.setDataBind(updateCase: "recommend", model: self.versionEntity)
+        } else {
+            updateAlertView.isHidden = true
+            self.showNextPage()
+        }
+    }
+    
+    func showNextPage() {
+        if UserManager.shared.hasAccessToken {
+            if UserManager.shared.isPostMemeber {
+                postLoginAPI(socialAccessToken: UserManager.shared.getAccessToken, socialType: UserManager.shared.getSocialType)
+            } else {
+                presentToOnboardingView()
+            }
+        } else {
+            presentToLoginView()
+        }
+    }
+    
+    func presentToOnboardingView() {
+        let nav = StoryTellingViewController()
+        self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+    func presentToLoginView() {
+        let nav = LoginViewController()
+        self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+    func presentToHomeView() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let keyWindow = windowScene.windows.first else {
+            return
+        }
+        keyWindow.rootViewController = TabBarController()
+    }
+}
+
+extension SplashViewController: AlertDelgate {
+    
+    func forceButtonTapped() {
+        let result = self.openAppStore(urlStr: self.appStoreOpenUrlString)
+        switch result {
+        case .success:
+            break
+        case .failure(let error):
+            print("Failed to open App Store: \(error)")
+        }
+    }
+    
+    func recommendBackButtonTapped() {
+        self.dismiss(animated: false, completion: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                self.showNextPage()
+            }
+        })
+    }
+    
+    func recommendUpdateButtonTapped() {
+        let result = self.openAppStore(urlStr: self.appStoreOpenUrlString)
+        switch result {
+        case .success:
+            break
+        case .failure(let error):
+            print("Failed to open App Store: \(error)")
+        }
+    }
+}
+
+// MARK: - Network
 
 private extension SplashViewController {
     func postLoginAPI(socialAccessToken: String, socialType: String) {
@@ -66,36 +194,25 @@ private extension SplashViewController {
             }
         }
     }
+    
+    func getVersionAPI() {
+        AuthService.shared.getVersionAPI { networkResult in
+            switch networkResult {
+            case .success(let data):
+                if let data = data as? GenericResponse<VersionEntity> {
+                    if let listData = data.data {
+                        self.versionEntity = listData
+                    }
+                    if let comparisonResult = self.appVersion?.compare(self.versionEntity.iosVersion.forceUpdateVersion, options: .numeric).rawValue as? Int, let comparisonResult2 = self.appVersion?.compare(self.versionEntity.iosVersion.appVersion, options: .numeric).rawValue as? Int {
+                        self.showUpdateAlert(forceResult: comparisonResult, recommendResult: 0)
+                    }
+                }
+            case .requestErr, .serverErr:
+                break
+            default:
+                break
+            }
+        }
+    }
 }
 
-private extension SplashViewController {
-    func showNextPage() {
-        if UserManager.shared.hasAccessToken {
-            if UserManager.shared.isPostMemeber {
-                postLoginAPI(socialAccessToken: UserManager.shared.getAccessToken, socialType: UserManager.shared.getSocialType)
-            } else {
-                presentToOnboardingView()
-            }
-        } else {
-            presentToLoginView()
-        }
-    }
-    
-    func presentToOnboardingView() {
-        let nav = StoryTellingViewController()
-        self.navigationController?.pushViewController(nav, animated: true)
-    }
-    
-    func presentToLoginView() {
-        let nav = LoginViewController()
-        self.navigationController?.pushViewController(nav, animated: true)
-    }
-    
-    func presentToHomeView() {
-        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-              let keyWindow = windowScene.windows.first else {
-            return
-        }
-        keyWindow.rootViewController = TabBarController()
-    }
-}
