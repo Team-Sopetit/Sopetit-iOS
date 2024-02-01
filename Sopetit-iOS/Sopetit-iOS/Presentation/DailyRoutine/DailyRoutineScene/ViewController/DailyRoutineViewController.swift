d0712e733b199054fb570e0f0c374ccc92f5330a
frohsch <kminjoo1213@gmail.com>
2024-02-01 16:04:28 +0900

    [Fix] #121 - Add logic when isEditing it can't completed

--- Sopetit-iOS/Sopetit-iOS/Presentation/DailyRoutine/DailyRoutineScene/ViewController/DailyRoutineViewController.swift
+++ Sopetit-iOS/Sopetit-iOS/Presentation/DailyRoutine/DailyRoutineScene/ViewController/DailyRoutineViewController.swift
@@ -257,11 +257,13 @@ extension DailyRoutineViewController: UICollectionViewDelegateFlowLayout {
 extension DailyRoutineViewController: ConfirmDelegate {
     
     func tapButton(index: Int) {
-        completeRoutineId = dailyRoutineEntity.routines[index].routineID
-        if let iconURL = URL(string: dailyRoutineEntity.routines[index].iconImageURL) {
-            self.dailyCompleteBottomSheet.imageView.downloadedsvg(from: iconURL)
+        if isEditing == false {
+            completeRoutineId = dailyRoutineEntity.routines[index].routineID
+            if let iconURL = URL(string: dailyRoutineEntity.routines[index].iconImageURL) {
+                self.dailyCompleteBottomSheet.imageView.downloadedsvg(from: iconURL)
+            }
+            self.present(dailyCompleteBottomSheet, animated: false)
         }
-        self.present(dailyCompleteBottomSheet, animated: false)
     }
 }
 
